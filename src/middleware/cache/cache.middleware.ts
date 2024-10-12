ac9a0753723a02fff5c32e0418ff72a6e214fd52
inmorpher <omennnn62@gmail.com>
2024-10-12 19:10:44 -0400

    [Update]: Refactor and improve CacheMiddleware
    - Enhance caching logic for GET, POST, PUT, and DELETE methods
    - Implement more robust cache key generation
    - Add cache invalidation for related entries
    - Improve error handling and response caching mechanism

--- src/middleware/cache/cache.middleware.ts
+++ src/middleware/cache/cache.middleware.ts
@@ -4,12 +4,20 @@ import NodeCache from 'node-cache';
 import 'reflect-metadata';
 import { IMiddleware } from '../../common/middleware.interface';
 
+enum HttpMethod {
+	GET = 'GET',
+	POST = 'POST',
+	PUT = 'PUT',
+	DELETE = 'DELETE',
+}
+
 @injectable()
 export class CacheMiddleware implements IMiddleware {
-	private cache: NodeCache;
-
-	constructor(cache: NodeCache) {
-		this.cache = cache;
+	private scheme: string;
+	private cacheTTL: number;
+	constructor(private cache: NodeCache, scheme: string, cacheTTL: number = 60000) {
+		this.scheme = scheme;
+		this.cacheTTL = cacheTTL;
 	}
 
 	/**
@@ -23,16 +31,62 @@ export class CacheMiddleware implements IMiddleware {
 	 * @returns void - This function doesn't return anything directly, but may send a response if cached data is found.
 	 */
 	execute(req: Request, res: Response, next: NextFunction) {
-		const cacheKey = `users_${req.url}`;
+		const cacheKey = this.getCacheKey(req);
+		switch (req.method) {
+			case 'GET':
+				this.handleGet(cacheKey, req, res, next);
+				break;
+			case 'POST':
+			case 'PUT':
+			case 'DELETE':
+				this.handleWrite(cacheKey, req, res, next);
+				break;
+			default:
+				next();
+				break;
+		}
+	}
+
+	private getCacheKey(req: Request): string {
+		const queryString = Object.keys(req.query).length ? `_${JSON.stringify(req.query)}` : '';
+		const paramsString = Object.keys(req.params).length ? `_${JSON.stringify(req.params)}` : '';
+
+		return `$_${this.scheme}_${paramsString}${queryString}`;
+	}
+	private handleGet(cacheKey: string, req: Request, res: Response, next: NextFunction) {
 		const cachedData = this.cache.get(cacheKey);
 		if (cachedData) {
 			return res.json(cachedData);
 		}
-		(res as any).originalJson = res.json;
-		res.json = (body) => {
-			this.cache.set(cacheKey, body, 60000); // Cache for 1 minute
-			return (res as any).originalJson(body);
-		};
+		this.cacheResponse(cacheKey, res);
 		next();
+	}
+
+	private handleWrite(cacheKey: string, req: Request, res: Response, next: NextFunction) {
+		this.invalidateRelatedCache(req);
+		next();
+	}
+
+	private cacheResponse(cacheKey: string, res: Response) {
+		const originalJson = res.json;
+		res.json = (body: any) => {
+			// Check if the response is successful (status code 2xx)
+			if (res.statusCode >= 200 && res.statusCode < 300) {
+				this.cache.set(cacheKey, body, this.cacheTTL);
+			}
+			return originalJson.call(res, body);
+		};
+	}
+
+	private invalidateRelatedCache(req: Request) {
+		const currentCacheKey = this.getCacheKey(req);
+		this.cache.del(currentCacheKey);
+		const allUsersCacheKey = `$_${this.scheme}_`;
+		this.cache.del(allUsersCacheKey);
+		if (req.params.id) {
+			const userCacheKey = `$_${this.scheme}_{"id":"${req.params.id}"}`;
+			this.cache.del(userCacheKey);
+			console.log(`Deleted cache for user: ${userCacheKey}`);
+		}
 	}
 }
