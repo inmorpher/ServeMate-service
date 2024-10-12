54407b497a65a527877d35fc5c404ba143db9b58
inmorpher <omennnn62@gmail.com>
2024-10-12 19:13:22 -0400

    [Update]: Enhance App configuration and middleware setup
    - Implement conditional authentication middleware for production environment
    - Add CORS and cookie-parser middleware
    - Improve API routing structure
    - Implement exception filters
    - Add server initialization with logging

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Express, json, urlencoded } from 'express';
+import express, { Express, json, Router, urlencoded } from 'express';
 import { Server } from 'http';
 import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
 import { ENV } from '../env';
+import { AuthenticationController } from './controllers/auth/auth.controller';
 import { UserController } from './controllers/users/users.controller';
 import { IExceptionFilter } from './errors/exception.filter.interface';
+import { AuthMiddleware } from './middleware/auth/auth.middleware';
 import { ILogger } from './services/logger/logger.service.interface';
 import { TYPES } from './types';
 
@@ -17,6 +20,8 @@ export class App {
 	constructor(
 		@inject(TYPES.ILogger) private logger: ILogger,
 		@inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
+		@inject(TYPES.AuthMiddleware) private authMiddleware: AuthMiddleware,
+		@inject(TYPES.AuthenticationController) private authController: AuthenticationController,
 		@inject(TYPES.UserController) private userController: UserController
 	) {
 		this.app = express();
@@ -30,11 +35,33 @@ export class App {
 			res.setHeader('Permission-Policy', 'geolocation=(), microphone=(), camera=()');
 			next();
 		});
+		console.log(ENV.PRODUCTION);
+
+		if (ENV.PRODUCTION) {
+			this.app.use('/api', (req, res, next) => {
+				if (req.path.startsWith('/auth')) {
+					return next();
+				}
+				this.authMiddleware.execute(req, res, next);
+			});
+		}
+		// this.app.use('/api', (req, res, next) => {
+		// 	if (req.path.startsWith('/auth')) {
+		// 		return next();
+		// 	}
+		// 	this.authMiddleware.execute(req, res, next);
+		// });
 	}
 
 	private useRoutes(): void {
+		const apiRouter = Router();
+
+		apiRouter.use('/auth', this.authController.router);
+		apiRouter.use('/users', this.userController.router);
+
 		this.app.use(cors());
-		this.app.use('/api', this.userController.router);
+		this.app.use(cookieParser());
+		this.app.use('/api', apiRouter);
 	}
 
 	private useExceptionFilters(): void {
@@ -46,7 +73,6 @@ export class App {
 		this.useRoutes();
 		this.useExceptionFilters();
 		this.server = this.app.listen(this.port);
-		this.logger.setContext('App');
 		this.logger.log(`Server is running on port ${this.port}`);
 	}
 }
