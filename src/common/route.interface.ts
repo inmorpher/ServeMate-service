537d1d9d61c482b56949bf53cc652649460b2d46
inmorpher <omennnn62@gmail.com>
2024-11-03 15:18:42 -0500

    [Add](Route interface): TypedRequest interface for enhanced request typing
    - Introduced new TypedRequest interface in src/common/route.interface.ts
    - Implemented generic types for params, query, and body in TypedRequest
    - Improved type safety for incoming request data
    - Enhanced developer experience with better IntelliSense support
    
    This change allows for more precise typing of request objects,
    reducing potential runtime errors and improving code quality.

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,11 +1,32 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
 import { IMiddleware } from './middleware.interface';
+
+/**
+ * Extends the Express Request interface to provide typed parameters, query, and body.
+ * This interface allows for better type checking and autocompletion when working with Express requests.
+ *
+ * @template TParams - The type for route parameters (defaults to an empty object)
+ * @template TQuery - The type for query parameters (defaults to an empty object)
+ * @template TBody - The type for request body (defaults to an empty object)
+ *
+ * @extends {Request} - Extends the Express Request interface
+ */
+export interface TypedRequest<TParams = {}, TQuery = {}, TBody = {}> extends Request {
+	/** The typed request body */
+	body: TBody;
+	/** The typed query parameters, merged with ParsedQs for compatibility with Express */
+	query: TQuery & ParsedQs;
+	/** The typed route parameters, merged with ParamsDictionary for compatibility with Express */
+	params: TParams & ParamsDictionary;
+}
 
 /**
  * Represents the structure of a controller route in the application.
  * This interface defines the properties needed to set up a route in an Express.js application.
  */
-export interface IControllerRoute {
+export interface IControllerRoute<TParams, TQuery, TBody> {
 	/**
 	 * The URL path for the route.
 	 */
@@ -17,7 +38,7 @@ export interface IControllerRoute {
 	 * @param res - The Express Response object.
 	 * @param next - The Express NextFunction for passing control to the next middleware.
 	 */
-	func: (req: Request, res: Response, next: NextFunction) => void;
+	func: (req: TypedRequest<TParams, TQuery, TBody>, res: Response, next: NextFunction) => void;
 
 	/**
 	 * The HTTP method for the route.
