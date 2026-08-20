987e26616c1dcd000c40536b22a1feb4bac82397
inmorpher <omennnn62@google.com>
2026-08-20 16:04:27 -0400

    feat: enhance drink and food item schemas with additional sorting options

--- src/food-items/dto/food-query.dto.ts
+++ src/food-items/dto/food-query.dto.ts
@@ -1,9 +1,27 @@
 import { FoodCategory, FoodType } from '@prisma/client';
 import { z } from 'zod';
+import { FoodItemSortOptions } from './food-base.dto';
 export const searchFoodItemsSchema = z.object({
   page: z.coerce.number().int().positive().default(1),
   pageSize: z.coerce.number().int().positive().max(100).default(10),
-  sortBy: z.string().default('id'),
+  sortBy: z
+    .enum([
+      FoodItemSortOptions.ID,
+      FoodItemSortOptions.NAME,
+      FoodItemSortOptions.PRICE,
+      FoodItemSortOptions.POPULARITY,
+      FoodItemSortOptions.IS_AVAILABLE,
+      FoodItemSortOptions.CREATED_AT,
+      FoodItemSortOptions.UPDATED_AT,
+      FoodItemSortOptions.TYPE,
+      FoodItemSortOptions.CATEGORY,
+      FoodItemSortOptions.PREPARATION_TIME,
+      FoodItemSortOptions.SPICY_LEVEL,
+      FoodItemSortOptions.CALORIES,
+      FoodItemSortOptions.IS_VEGAN,
+      FoodItemSortOptions.IS_GLUTEN_FREE,
+    ])
+    .default(FoodItemSortOptions.ID),
   sortOrder: z.enum(['asc', 'desc']).default('asc'),
   name: z.string().optional(),
   category: z.enum(FoodCategory).optional(),
